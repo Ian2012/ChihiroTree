244a7afd42d3e54ffbf00e0700d69e87f21f6b5b
khor <crisgarta8@gmail.com>
2020-12-22 12:42:30 -0500

    Creación de la clase Tree

--- main.py
+++ main.py
@@ -1,22 +1,47 @@
-def cargar(ruta: str):
-    file = open(ruta)
-    content = file.readlines()
+class Tree:
 
-    board = str()
-    mapa = list()
-    for line in content:
-        if line[0] == '#':
-            continue
+    def __init__(self, ruta):
+        self.__mapa = list()
+        self.__x = int()
+        self.__y = int()
 
-        board += line
-        line = line.replace(",", "")
-        temporal_line = list()
-        for char in line:
-            if char != "\n":
-                temporal_line.append(int(char))
-        mapa.append(temporal_line)
+        self.load_file(ruta)
+        self.children = list()
+        print(self.__x, self.__y)
+        print(self.__mapa[self.__y][self.__x])
+        for line in self.__mapa:
+            print(line)
+        # self.load_children()
 
-    return mapa
+    def load_file(self, ruta: str):
+        file = open(ruta)
+        content = file.readlines()
+
+        board = str()
+        mapa = list()
+        heigth = 0
+        for line in content:
+            if line[0] == '#':
+                continue
+
+            board += line
+            line = line.replace(",", "")
+            temporal_line = list()
+            width = 0
+
+            for char in line:
+                if char != "\n":
+                    if char == '4':
+                        self.__x = width
+                        self.__y = heigth
+                    temporal_line.append(int(char))
+                width += 1
+            mapa.append(temporal_line)
+            heigth += 1
+        self.__mapa = mapa
+
+    def load_children(self):
+        pass
 
 
-print(cargar("map/map1.txt"))
+tree = Tree("map/map1.txt")
