33cbdef93f4743da516009ad474a686a4a12f303
Ian2012 <crisgarta8@gmail.com>
2021-01-10 16:09:22 -0500

    Archivo Search.py creado

---
+++ Search.py
@@ -0,0 +1,25 @@
+from Tree import Tree
+
+
+def bfs():
+    root = Tree("map/map1.txt")
+    qeue = list()
+    qeue.append(root)
+
+    # Mientras no esté vacío
+    while qeue:
+
+        node = qeue[0]
+
+        # print(node.depth)
+        if node.end:
+            print("\n")
+            print(node)
+            return node
+
+        node.load_children()
+        qeue.extend(node.children)
+
+        qeue.remove(node)
+
+    return "No se ha encontrado una solución"
--- main.py
+++ main.py
@@ -1,29 +1,4 @@
-from Tree import Tree
-from Utilities import init
+import Search
+import Utilities
 
-
-def busqueda_por_amplitud():
-    root = Tree("map/map1.txt")
-    qeue = list()
-    qeue.append(root)
-
-    # Mientras no esté vacío
-    while qeue:
-
-        node = qeue[0]
-
-        # print(node.depth)
-        if node.end:
-            print("\n")
-            print(node)
-            return node
-
-        node.load_children()
-        qeue.extend(node.children)
-
-        qeue.remove(node)
-
-    return "No se ha encontrado una solución"
-
-
-init(busqueda_por_amplitud)
+Utilities.init(Search.bfs)
